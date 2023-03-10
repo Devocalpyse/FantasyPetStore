ba47c746d6d26a3c3a214ee9af0852151d7b803c
Devocalpyse <ngl2d9uvg@relay.firefox.com>
2023-03-10 09:35:18 -0800

    app.ts and default layouts setup

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from 'express'
+import path from 'path'
+import morgan from 'morgan'
+
+// Basic set-up
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../src/public')));
+
+// View engine = Handlebars
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, "../src/views"));
+app.set('view options', { layout: 'layout' });
+
+// Routing middleware (to-be-done)
+
+    // Bad request routing:
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        res.status(404).render('error', {
+            message: "This is not the URL you are looking for!"
+        });
+    })
+
+// Syncing our database (to-be-done)
+
+// Server: 3000
+app.listen(3000)
